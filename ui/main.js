6b04da4fd11f682b634aaff426569a9754ba783e
depressed-Iceberg <pcut856@gmail.com>
2016-10-29 10:49:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,15 +2,4 @@
 
 
     
-     <div class="center">
-            <img id = 'kek' src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSUoQQz-mKtvOyfIvGdeYmfagxz_W2KJCAxBreXMa_iFpS94qA98Mu_ww" class="img-medium"/>
-          
-        </div>
-            <hr>
-        <div class="center text-big bold">
-            what's this?Kekels OwO
-            
-        </div>
-        <div class="center">
-            <img src ="https://upload.wikimedia.org/wikipedia/en/e/ee/Harambe_with_boy.jpg"/>
-        </div>
+    
