925022e84525742d813e66c0229c8518e6da1516
depressed-Iceberg <pcut856@gmail.com>
2016-10-29 10:48:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,16 @@
  document.getElementById("demo").innerHTML="Hello World";
 
+
+    
+     <div class="center">
+            <img id = 'kek' src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSUoQQz-mKtvOyfIvGdeYmfagxz_W2KJCAxBreXMa_iFpS94qA98Mu_ww" class="img-medium"/>
+          
+        </div>
+            <hr>
+        <div class="center text-big bold">
+            what's this?Kekels OwO
+            
+        </div>
+        <div class="center">
+            <img src ="https://upload.wikimedia.org/wikipedia/en/e/ee/Harambe_with_boy.jpg"/>
+        </div>
