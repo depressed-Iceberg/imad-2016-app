8dc8c5e187ce4ff8edc4c49b0d1c8745d5d5a21a
depressed-Iceberg <pcut856@gmail.com>
2016-11-12 17:41:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,47 @@
         <div class="center">
             <img src ="https://upload.wikimedia.org/wikipedia/en/e/ee/Harambe_with_boy.jpg"/>
         </div>*/
+var button = document.getElementById('counter');
+
+button.onclick = function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readystate === XMLHttpRequest.Done){
+            if (reuqest.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    };
+    request.open('GET','',true);
+    request.send(null);
+};
+var submit = document.getElementById('submit_btn');
+submit.onclick= function(){
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.steadystate === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list +='<li>'+names[i]+'</li>';
+                    
+                }
+                var ul= document.getElementById('namelist');
+                ul.innerHTML= list ;
+            }
+        }
+    };
+    
+    var nameInput = getEelementById('name');
+    var name = nameInput.value;
+    request.open('GET','',true);
+    request.send('null')
+}
