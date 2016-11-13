b5de2fb443093fe0e49a4ea637595820b2f42117
depressed-Iceberg <pcut856@gmail.com>
2016-11-13 20:42:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,10 +14,37 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParse.json());
+function createTemplate(data){}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync(input , salt, 100000, 512, 'sha512');
+    return ["pbkdf","10000",salt,hashed.toString('hex')].join('$');
+}
+
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input,'random-string');
+    res.send(hashedString);
+});
+
+app.post('/create-user',function(req,res){
+    //username,password
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2) ',[username, dbString],  function(err,result){
+       if(err){
+       res.status(500).send(err,toString());
+       }
+       else{
+       res.send('User successfully created:'+ username);
+       }
+    });
+    
+});
+
 var pool = new Pool(config);
 app.get('/test-db',function(req,res) {
  //make a select request 
@@ -31,15 +58,6 @@ app.get('/test-db',function(req,res) {
   });
 });
 
-app.get('/create-user',function(req,res){
-    //username,password
-    var salt = crypto.getRandomBytes(128).toString('hex');
-    var dbString = hash(password,salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2) ',[username, dbString],  function(err,result){
-        
-    })
-    
-});
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
