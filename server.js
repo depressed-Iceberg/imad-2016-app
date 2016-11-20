2033d6bf2609d8788d982f4b6bdd93347ea17447
depressed-Iceberg <pcut856@gmail.com>
2016-11-20 17:10:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,35 @@ var app = express();
 app.use(morgan('combined'));
 app.use(bodyParse.json());
 function createTemplate(data){}
-
+    var title = data.title;
+    var date=date.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate=
+    <html>
+     <head>
+     <title>
+     ${title}
+     </title>
+     <meta name ="viewport" content ="width=device-width,initial-scale=1"/>
+     <link href ="/ui/style.css"rel="stylesheet"/>
+     </head>
+     <body>
+         <div class="container">
+         <div>
+           <a href="/">Home</a>
+          </div>
+          <hr/>
+          <h3>
+             ${heading}
+          </h3>
+          <div>
+             ${date}
+          </div>
+          <div>
+            ${content}
+          </div>
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
