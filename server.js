66ab6ea6115b81426376fc374f35c93601980af7
depressed-Iceberg <pcut856@gmail.com>
2016-11-26 16:29:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,11 +97,11 @@ app.get('/', function (req, res) {
 app.get('/profile/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'project/profile', 'index.html'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'project', 'index.html'));
+app.get('/contact/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'project/contact', 'index.html'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'project', 'index.html'));
+app.get('/blog/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'project/blog', 'index.html'));
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'project', 'index.html'));
