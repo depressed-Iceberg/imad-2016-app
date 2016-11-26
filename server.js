32f3bbbeed3bf0cc5faadf258ecd0975d7cc2b55
depressed-Iceberg <pcut856@gmail.com>
2016-11-26 16:30:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,9 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'project', 'index.html'));
 });
+app.get('/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'project', 'index.html'));
+});
 app.get('/profile/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'project/profile', 'index.html'));
 });
